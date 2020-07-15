f73ca424b89ec1152298948f483087ae08cd493a
Daniel Lima de Oliveira <oliveiradaniel11@hotmail.com>
2020-07-14 22:13:07 -0300

    add products to test somes  event bind add property bind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = [
+    {id: 1, name: 'Caneta', price: 2.3},
+    {id: 2, name: 'Lapis', price: 1.0},
+    {id: 1, name: 'Borracha', price: 0.5},
+  ];
+  isScreenDetail = false;
+  productToDetail: Product;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showDetailProduct(product: Product): void {
+    this.isScreenDetail = true;
+    this.productToDetail = product;
+  }
+
+  deleteProduct(productToDelete: Product): void {
+    this.products = this.products.filter(product => product !== productToDelete);
+    this.isScreenDetail = false;
+  }
+}
